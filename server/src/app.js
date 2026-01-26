85fda4e713d78b5bbf838d7622b63c7875e44527
AnavilAbhishekOjha <ojhaanavilabhishek.ojha@gmail.com>
2026-01-26 14:14:42 +0530

    Add register route and fix app.js

--- server/src/app.js
+++ server/src/app.js
@@ -8,10 +8,20 @@ app.use(morgan('combine'));
 app.use(bodyParse.json())
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Tab Tracker!');
+});
+
 app.get('/status', (req, res) => {
     res.send({
         message: 'hello world!'
     })
+});
+
+app.post('/register', (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email} Your user was registered!`
+    });
 });
 
 app.listen(process.env.PORT || 8081);
